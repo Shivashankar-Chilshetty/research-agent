01a91fe100d4dbff8571c9fe03f9b70f61bcff0c
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2025-12-14 11:05:07 +0530

    Implemented responder node

---
+++ src/graph.ts
@@ -0,0 +1,42 @@
+import { AIMessage } from '@langchain/core/messages';
+import { llm } from './model';
+import { graphState, questionAnswerSchema } from './state';
+import { StateGraph } from '@langchain/langgraph';
+//import { searchExecutor } from './tools';
+
+
+async function responder(state: typeof graphState.State) {
+    const currentDateTime = new Date().toLocaleString('sv-SE');
+
+    const SYSTEM_PROMPT = `You are an expert researcher.
+        Current time: ${currentDateTime}
+
+        1. Provide a detailed ~250 word answer.
+        2. Reflect and critique your answer. Be severe to maximize improvement.
+        3. Recommend max 3 search queries to research information and improve your answer.`;
+
+    // Wrap the LLM with structured output(schema) capabilities
+    const llmWithStructure = llm.withStructuredOutput(questionAnswerSchema);
+
+    const response = await llmWithStructure.invoke([
+        {
+            role: 'system',
+            content: SYSTEM_PROMPT,
+        },
+        ...state.messages,
+        {
+            role: 'system',
+            content: `Reflect on the user's original question and the actions taken thus far. Respond using structured output.`,
+        },
+    ]);
+
+    return {
+        messages: [new AIMessage(JSON.stringify(response))], //storing the message in state as AIMessage
+        iteration: 0,
+    };
+}
+
+
+export const graph = new StateGraph(graphState)
+    .addNode('responder', responder)
+
--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,23 @@
 import { Annotation, MessagesAnnotation } from "@langchain/langgraph";
+import z from 'zod';
+
+//critique schema
+const reflectionSchema = z.object({
+    missing: z.string().describe('Critique of what is missing.'), //what is lacking
+    superfluous: z.string().describe('Critique of what is superfluous'), //what is unnecessary
+});
+
+export const questionAnswerSchema = z.object({
+    answer: z.string().describe('~250 word detailed answer to the question.'),
+    reflection: reflectionSchema,
+    searchQueries: z
+        .array(z.string())
+        .describe(
+            '1-3 search queries for researching improvements to address the critique of your current answer.'
+        ),
+});
+
+export type QuestionAnswer = z.infer<typeof questionAnswerSchema>;
 
 export const graphState = Annotation.Root({
     //stores message history(AI msg, User msg etc)
